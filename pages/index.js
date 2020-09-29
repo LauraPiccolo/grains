8902e3a31e0f077369a03ae0aef23cd0bc7d6c03
lukas eberlein <>
2020-09-29 14:49:12 +0200

    added api...

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,37 @@
 import Layout from '../components/layout'
+import { getHome } from '../lib/api'
 
-export default function Home() {
-  return (
-    <>
-    <Layout>
-        <main>
-            <section>
-            Hey this is Version Nr. 2
-            </section>
-            <section className="" >
-                <div className="textarea"></div>
-                <div className="imagegrid">
-                    <article className="imagegrid__item">
+const Home = ({ content }) => {
 
-                    </article>
-                </div>
-            </section>
-        </main>
-    </Layout>
-    </>
-  )
+    console.log(content)
+
+    return (
+        <>
+        <Layout>
+            <main>
+                <section>
+                Hey this is Version Nr. 2
+                </section>
+                <section className="" >
+                    <div className="textarea"></div>
+                    <div className="imagegrid">
+                        <article className="imagegrid__item">
+
+                        </article>
+                    </div>
+                </section>
+            </main>
+        </Layout>
+        </>
+    )
+}
+
+export default Home
+
+export async function getStaticProps({ preview = null }) {
+    const content = (await getHome(preview)) || []
+    return {
+        props: { content },
+    }
 }
 
