2ece4616a8084e5d84139e99669296c69999b55c
LauraPiccolo <hello.laurapiccolo@gmail.com>
2020-09-30 13:21:04 +0200

    started grid component

--- components/blocks/block/grid.js
+++ components/blocks/block/grid.js
@@ -4,11 +4,33 @@ import Image from '../../utils/image'
 
 const SectionGrid = ({ content }) => {
 
-    console.log(content.items[0])
+    console.log(content.items[0]);
 
     return (
-        <div className="section--grid">
-            <Image data={content.items[0].images[0]} mode={""} />
+        <div className="section--grid" style={{
+            gridTemplateColumns: `repeat(12, 1fr)`,
+            gridTemplateRows: `repeat(${content.items.length}, auto)`
+        }}>
+            {
+                content.items.map((raw, index) => {
+                    raw.images.length === 1 ? (
+                        <Image data={raw.images[0]} mode={""} class={raw.mode} style={{
+                            gridRowStart:index+1, gridRowEnd:index+2
+                        }}/>
+                    ):(
+                        <>
+                        <Image data={raw.images[0]} mode={""} class={raw.mode} style={{
+                            gridRowStart:index+1, gridRowEnd:index+2
+                        }}/>
+                        <Image data={raw.images[1]} mode={""} class={raw.mode} style={{
+                            gridRowStart:index+1, gridRowEnd:index+2
+                        }}/>
+                        </>
+                    )
+                })
+                
+            }
+            
         </div>
     )
 }
--- components/utils/image.js
+++ components/utils/image.js
@@ -9,7 +9,6 @@ const resize = (image, option) => {
 
 const Image = (props) => {
     const data = props.data 
-
     var url = data.filename  
     // const alt = data.alt
     let dimensions = {
@@ -33,7 +32,7 @@ const Image = (props) => {
     }
 
     return (
-        <div className={["image__container","mode--"+props.mode].join(' ')} style={{paddingBottom: padding}}>
+        <div className={["image__container","mode--"+props.mode].join(' '),`griditem--${props.class}`} style={{paddingBottom: padding, ...props.style}}>
             <div className="image__wrapper">
                 <img 
                     data-sizes="auto"
