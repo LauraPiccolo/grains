a0d555c27067559e8e381efd209c908659133309
LauraPiccolo <hello.laurapiccolo@gmail.com>
2020-09-30 11:38:44 +0200

    Added Contact top component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout'
 import Sections from '../components/sections/sections'
 import { getHome } from '../lib/api'
+import Mailchimp from 'react-mailchimp-form'
 
 const Home = ({ content }) => {
 
@@ -11,6 +12,40 @@ const Home = ({ content }) => {
         <Layout>
             <main>
                 <div className="intro">
+                    <div className="intro__address">
+                        Sanderstr. 29–30
+                        <br/>12047 Berlin, Germany
+                    </div>
+
+                    <div className="intro__contact">
+                        info@kopfundkragen.eu 
+                        <br/>+49 (0)30 89 56 69 41
+                        <br/>@kopfundkragen_fotografie
+                    </div>
+
+                    <div className="intro__newsletter">
+                        Newsletter
+                        <Mailchimp
+                        action='https://googlemail.us4.list-manage.com/subscribe/post?u=42451f8640431cce08fcfc879&amp;id=70d98ca351'
+                        fields={[
+                        {
+                            name: 'email',
+                            placeholder: 'E-Mail Adresse eingeben',
+                            type: 'email',
+                            required: true
+                        }]}
+                        messages = {
+                            {
+                                // sending: "Sending...",
+                                // success: "Thank you for subscribing!",
+                                // error: "An unexpected internal error has occurred.",
+                                // empty: "You must write an e-mail.",
+                                // duplicate: "Too many subscribe attempts for this email address",
+                                button: "Abonnieren"
+                            }
+                        }
+                        />
+                    </div>
                 </div>
                 <div className="anker">
                     <Sections content={content} />
